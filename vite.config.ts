92583dd9119527af1cd5a8fa08da679b1f77f42e
HojinLee <hojin5633@gmail.com>
2024-08-21 21:19:14 +0900

    chore: newtab vite 빌드 설정
    - newtab.html을  root에 위치시켜 dist/newtab.html에 생성되도록
    
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>
    Co-authored-by: saeeng <saeeng.opt@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        newtab: "./newtab.html",
+      },
+    },
+  },
   plugins: [react()],
-})
+});
