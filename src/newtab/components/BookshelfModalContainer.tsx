c00fedcd326aa502a931b9bd666560cace38edfe
HojinLee <hojin5633@gmail.com>
2024-08-31 11:51:50 +0900

    feat: react-moveable 추가, 드래그 동작 추가
    Co-authored-by: saeeng <saeeng.opt@gmail.com>
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>

--- src/newtab/components/BookshelfModalContainer.tsx
+++ src/newtab/components/BookshelfModalContainer.tsx
@@ -1,25 +1,54 @@
-import type { FC } from "react";
+import { useRef, type FC } from "react";
 import { bookshelfModalStore } from "../store/bookshelfModal";
 import Bookshelf from "./Bookshelf";
+import Moveable from "react-moveable";
 
 const BookshelfModalContainer: FC = () => {
   const { bookshelfModals } = bookshelfModalStore();
+
   return (
     <div>
       {Object.entries(bookshelfModals).map(([key, value]) => {
-        return (
-          <div
-            className="absolute flex size-[500px] rounded border bg-white"
-            style={{
-              top: "50%",
-              left: "50%",
-            }}
-            key={key}
-          >
-            <Bookshelf id={value.id} />
-          </div>
-        );
+        return <BookshelfModal key={key} id={value.id} />;
       })}
+    </div>
+  );
+};
+
+const BookshelfModal = ({ id }: { id: string }) => {
+  const targetRef = useRef<HTMLDivElement>(null);
+  const dragTargetRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="container">
+      <div
+        className="absolute flex size-[500px] flex-col rounded-2xl border bg-neutral-50"
+        style={{
+          top: "10px",
+          left: 0,
+        }}
+        ref={targetRef}
+      >
+        <div
+          ref={dragTargetRef}
+          className="flex h-12 w-full items-center  rounded-t-2xl bg-neutral-300 p-2 hover:border-b"
+        >
+          HEADER
+        </div>
+        <Bookshelf id={id} />
+      </div>
+      <Moveable
+        target={targetRef}
+        dragTarget={dragTargetRef}
+        draggable={true}
+        throttleDrag={1}
+        edgeDraggable={false}
+        startDragRotate={0}
+        throttleDragRotate={0}
+        onDrag={(e) => {
+          e.target.style.transform = e.transform;
+        }}
+      />
     </div>
   );
 };
