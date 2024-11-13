e9ca0cd2b8a094a77113b748cea4eb77d59675b6
HojinLee <hojin5633@gmail.com>
2024-11-13 22:07:46 +0900

    feat: Grid 내 버튼의 크기를 grid size에 맞도록 수정

--- src/newtab/components/FileView.tsx
+++ src/newtab/components/FileView.tsx
@@ -10,33 +10,28 @@ type Props = {
 /** @TODO favicon 그리기 (원래 코드에서 안옮겼음) */
 const FileView = ({ file, onMouseDown, style }: Props) => {
   return (
-    <div onMouseDown={onMouseDown} style={style}>
-      <div
-        className="flex justify-center bg-none"
+    <div
+      onMouseDown={onMouseDown}
+      style={{
+        ...style,
+        gridRow: file.row || "auto",
+        gridColumn: file.col || "auto",
+      }}
+      className="h-item w-item"
+    >
+      <button
+        className="flex size-full flex-col items-center"
         style={{
-          gridRow: file.row || "auto",
-          gridColumn: file.col || "auto",
+          border: "1px solid transparent",
         }}
       >
-        <div className="flex h-full flex-col items-center gap-2">
-          <button
-            className="h-item w-item"
-            style={{
-              padding: "8px",
-              border: "1px solid transparent",
-              width: "70px",
-              height: "90px",
-            }}
-          >
-            <div className="text-[48px] text-yellow-500">
-              {file.title.charAt(0)}
-            </div>
-            <p className="line-clamp-2 transform-none overflow-hidden text-ellipsis break-words text-xs leading-5 tracking-[.2px]">
-              {file.title}
-            </p>
-          </button>
+        <div className="flex  flex-[2] items-center text-[48px] leading-[48px] text-yellow-500">
+          {file.title.charAt(0)}
         </div>
-      </div>
+        <p className="line-clamp-2 flex-[1] transform-none overflow-hidden text-ellipsis break-words px-2 pb-1 text-xs leading-5 tracking-[.2px]">
+          {file.title}
+        </p>
+      </button>
     </div>
   );
 };
