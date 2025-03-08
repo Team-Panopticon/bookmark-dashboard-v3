5c6d2ef91cb9202d50065d0880027d10f3093b52
HojinLee <hojin5633@gmail.com>
2025-03-08 11:27:13 +0900

    refactor: DraggingFile 분리
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>

---
+++ src/newtab/components/DraggingFile.tsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { useEventHandler } from "../hooks/useEventHandler";
+import { rootStore } from "../store/rootStore";
+import { Z_INDEX } from "../utils/constant";
+import BookmarkView from "./BookmarkView";
+
+const DraggingFile = () => {
+  const { dragAndDrop = {} } = rootStore();
+  const {
+    bookmarkEventHandler: { handleMouseUpBookmark },
+  } = useEventHandler({});
+  const { fileElement, offsetBetweenStartPointAndFileLeftTop, bookmark } =
+    dragAndDrop;
+
+  const [{ x, y }, setDraggingFilePosition] = useState<{
+    x?: number;
+    y?: number;
+  }>({ x: undefined, y: undefined });
+
+  useEffect(() => {
+    const handleMouseDown = (event: MouseEvent) => {
+      if (!fileElement || !offsetBetweenStartPointAndFileLeftTop) return;
+
+      setDraggingFilePosition({
+        x: event.pageX - offsetBetweenStartPointAndFileLeftTop.x,
+        y: event.pageY - offsetBetweenStartPointAndFileLeftTop.y,
+      });
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!fileElement || !offsetBetweenStartPointAndFileLeftTop) return;
+
+      setDraggingFilePosition({
+        x: event.pageX - offsetBetweenStartPointAndFileLeftTop.x,
+        y: event.pageY - offsetBetweenStartPointAndFileLeftTop.y,
+      });
+    };
+
+    const handleMouseUp = () => {
+      setDraggingFilePosition({
+        x: undefined,
+        y: undefined,
+      });
+
+      document.removeEventListener("mousedown", handleMouseDown);
+    };
+
+    document.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("mouseup", handleMouseUp);
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [fileElement, offsetBetweenStartPointAndFileLeftTop]);
+
+  if (!bookmark) return null;
+  if (x === undefined && y === undefined) return null;
+
+  return (
+    <BookmarkView
+      bookmark={bookmark}
+      style={{
+        position: "absolute",
+        top: y,
+        left: x,
+        zIndex: Z_INDEX.DRAGGING_FILE,
+        pointerEvents: "none",
+      }}
+      onMouseUp={(e) => {
+        console.log("draging file mouse up");
+        handleMouseUpBookmark(e, bookmark);
+      }}
+    />
+  );
+};
+
+export default DraggingFile;
--- src/pages/Desktop.tsx
+++ src/pages/Desktop.tsx
@@ -1,11 +1,10 @@
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useCallback, useEffect } from "react";
 import Bookshelf from "../newtab/components/Bookshelf";
 import FolderManager from "../newtab/components/FolderManager";
-import BookmarkView from "../newtab/components/BookmarkView";
 import ContextMenu from "../newtab/components/ContextMenu";
 import { rootStore } from "../newtab/store/rootStore";
 import { useEventHandler } from "../newtab/hooks/useEventHandler";
-import { Z_INDEX } from "../newtab/utils/constant";
+import DraggingFile from "../newtab/components/DraggingFile";
 
 const DESKTOP_TIMESTAMP_ID = `${Date.now()}`;
 
@@ -44,86 +43,10 @@ const Desktop: FC = () => {
           timestamp={DESKTOP_TIMESTAMP_ID}
         ></Bookshelf>
       )}
-      {/* <CreateFolderModal></CreateFolderModal> */}
       <FolderManager />
       <ContextMenu />
-      {/* <UpdateModal /> */}
-      {/* <Tooltip /> */}
     </div>
   );
 };
 
 export default Desktop;
-
-const DraggingFile = () => {
-  // todo : rootStore 에서 값 가져오는걸로 변경
-  const { dragAndDrop = {} } = rootStore();
-  const {
-    bookmarkEventHandler: { handleMouseUpBookmark },
-  } = useEventHandler({});
-  const { fileElement, offsetBetweenStartPointAndFileLeftTop, bookmark } =
-    dragAndDrop;
-
-  const [{ x, y }, setDraggingFilePosition] = useState<{
-    x?: number;
-    y?: number;
-  }>({ x: undefined, y: undefined });
-
-  useEffect(() => {
-    const handleMouseDown = (event: MouseEvent) => {
-      if (!fileElement || !offsetBetweenStartPointAndFileLeftTop) return;
-
-      setDraggingFilePosition({
-        x: event.pageX - offsetBetweenStartPointAndFileLeftTop.x,
-        y: event.pageY - offsetBetweenStartPointAndFileLeftTop.y,
-      });
-    };
-
-    const handleMouseMove = (event: MouseEvent) => {
-      if (!fileElement || !offsetBetweenStartPointAndFileLeftTop) return;
-
-      setDraggingFilePosition({
-        x: event.pageX - offsetBetweenStartPointAndFileLeftTop.x,
-        y: event.pageY - offsetBetweenStartPointAndFileLeftTop.y,
-      });
-    };
-
-    const handleMouseUp = () => {
-      setDraggingFilePosition({
-        x: undefined,
-        y: undefined,
-      });
-
-      document.removeEventListener("mousedown", handleMouseDown);
-    };
-
-    document.addEventListener("mousedown", handleMouseDown);
-    document.addEventListener("mouseup", handleMouseUp);
-    document.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
-  }, [fileElement, offsetBetweenStartPointAndFileLeftTop]);
-
-  if (!bookmark) return null;
-  if (x === undefined && y === undefined) return null;
-
-  return (
-    <BookmarkView
-      bookmark={bookmark}
-      style={{
-        position: "absolute",
-        top: y,
-        left: x,
-        zIndex: Z_INDEX.DRAGGING_FILE,
-        pointerEvents: "none",
-      }}
-      onMouseUp={(e) => {
-        console.log("draging file mouse up");
-        handleMouseUpBookmark(e, bookmark);
-      }}
-    />
-  );
-};
