88b02f95922c6c378d881d8636147ee24f725df6
saewoong.jeon <saewoong.jeon@42dot.ai>
2025-03-12 21:05:12 +0900

    reset tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -13,6 +13,11 @@ export default defineConfig(({mode}) => {
       },
       minify: mode === "development" ? false : true,
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    },
   };
 });
