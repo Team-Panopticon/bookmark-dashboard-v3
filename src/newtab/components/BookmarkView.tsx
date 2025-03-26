5ef76871777e2a87f96a37fee22d214fe5c0c52f
saewoong.jeon <saewoong.jeon@42dot.ai>
2025-03-26 20:17:47 +0900

    feat: bookmark view

--- src/newtab/components/BookmarkView.tsx
+++ src/newtab/components/BookmarkView.tsx
@@ -1,5 +1,5 @@
-import { CSSProperties, useEffect, useRef, useState } from "react";
-import { BookmarkType, type Bookmark } from "../../types/store";
+import {CSSProperties, useEffect, useRef, useState} from "react";
+import {BookmarkType, type Bookmark} from "../../types/store";
 import FolderImage from "../../assets/folder.svg";
 
 type Props = {
@@ -13,6 +13,12 @@ type Props = {
   isEdit?: boolean;
   setIsEdit?: (isEdit: boolean) => void;
 };
+declare module "react" {
+  interface CSSProperties {
+    "field-sizing"?: "fixed" | "content";
+    fieldSizing?: "fixed" | "content";
+  }
+}
 
 /** @TODO favicon 그리기 (원래 코드에서 안옮겼음) */
 const BookmarkView = ({
@@ -68,76 +74,92 @@ const BookmarkView = ({
         ...style,
         gridRow: bookmark.row || "auto",
         gridColumn: bookmark.col || "auto",
-        border: focused ? "1px solid black" : "",
       }}
       onKeyDown={(e) => {
         if (e.code === "Enter" && isEdit === false) {
           setIsEdit?.(true);
         }
       }}
-      className="h-item w-item overflow-visible"
+      className="h-item w-item select-none overflow-visible"
       onDoubleClick={() => onDoubleClick?.(bookmark)}
     >
       <button
-        className="relative flex size-full flex-col items-center"
+        className="relative flex size-full flex-col items-center gap-1 "
         ref={containerRef}
         style={{
           border: "1px solid transparent",
         }}
       >
-        <div className="flex  flex-[2] items-center text-[48px] leading-[48px] text-yellow-500">
+        <div
+          className={`flex size-[68px] shrink-0 grow-0 items-center justify-center rounded-md ${
+            focused ? "bg-[#E6E6E6]" : ""
+          }`}
+        >
           {bookmark.type === BookmarkType.FOLDER ? (
-            <div className="flex flex-col">
+            <div className={`flex h-full items-center  p-2 `}>
               <img
                 src={FolderImage}
                 width={56}
                 height={56}
-                style={{ pointerEvents: "none" }}
+                style={{pointerEvents: "none"}}
               />
             </div>
           ) : (
-            <>{bookmark.title.charAt(0)}</>
+            <div className="text-[48px]">{bookmark.title.charAt(0)}</div>
           )}
         </div>
-        {!isEdit ? (
-          <p className="line-clamp-2 flex-[1] transform-none overflow-hidden text-ellipsis break-words px-2 pb-1 text-xs leading-5 tracking-[.2px]">
-            {newTitle}
-          </p>
-        ) : (
-          <textarea
-            ref={inputRef}
-            className="h-[140px] w-[150px] flex-[1] transform-none break-words px-2 pb-1 text-xs leading-5 tracking-[.2px]"
-            onChange={(e) => {
-              setNewTitle(e.target.value.replace(/[\n|\r\n|\r|]/g, ""));
-            }}
-            rows={3}
-            value={newTitle}
-            onBlur={(e) => {
-              // Escape 키로 인한 blur 이벤트 방지
-              if (e.relatedTarget === containerRef.current) {
-                e.preventDefault();
-                return;
-              }
-              saveTitle();
-            }}
-            onKeyDown={async (e) => {
-              e.stopPropagation();
 
-              if (e.code === "Enter") {
+        <div className="max-w-[86px] ">
+          {!isEdit ? (
+            <p
+              className={`line-clamp-2 overflow-hidden text-ellipsis break-words text-center text-xs leading-4`}
+            >
+              <span
+                className={`${
+                  focused ? "bg-[#0065E1]  text-white" : ""
+                } break-words rounded  box-decoration-clone px-1 py-px leading-3 `}
+              >
+                {newTitle}
+              </span>
+            </p>
+          ) : (
+            <textarea
+              ref={inputRef}
+              style={{
+                fieldSizing: "content",
+              }}
+              className="line-clamp-3 max-w-[88px] resize-none break-words  rounded-sm border border-[#5BA1FA]  bg-[#B3D7FE] px-0.5 caret-white outline-none ring ring-[#81B5FB]"
+              onChange={(e) => {
+                setNewTitle(e.target.value.replace(/[\n|\r\n|\r|]/g, ""));
+              }}
+              value={newTitle}
+              onBlur={(e) => {
+                // Escape 키로 인한 blur 이벤트 방지
+                if (e.relatedTarget === containerRef.current) {
+                  e.preventDefault();
+                  return;
+                }
                 saveTitle();
-                return;
-              }
+              }}
+              onKeyDown={async (e) => {
+                e.stopPropagation();
 
-              if (e.code === "Escape") {
-                e.preventDefault();
-                setNewTitle(bookmark.title);
-                setIsEdit?.(false);
-                containerRef.current?.focus();
-                return;
-              }
-            }}
-          />
-        )}
+                if (e.code === "Enter") {
+                  saveTitle();
+                  return;
+                }
+
+                if (e.code === "Escape") {
+                  e.preventDefault();
+                  setNewTitle(bookmark.title);
+                  setIsEdit?.(false);
+                  containerRef.current?.focus();
+                  return;
+                }
+              }}
+            />
+          )}
+        </div>
       </button>
     </div>
   );
