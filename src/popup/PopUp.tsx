075e54c2104e168384e066d71e7014815ba68f5b
HojinLee <hojin5633@gmail.com>
2025-04-05 12:01:31 +0900

    feat: popup 추가
    Co-authored-by: juyoungpark718 <juyoungpark718@users.noreply.github.com>
    Co-authored-by: saeeng <saeeng.opt@gmail.com>

---
+++ src/popup/PopUp.tsx
@@ -0,0 +1,37 @@
+import { version } from "../../package.json";
+
+const PopUp = () => {
+  return (
+    <div className="flex w-[260px] flex-col bg-white p-3 text-xs shadow-md backdrop-blur-md">
+      <div className="flex flex-col gap-1">
+        <div className="flex flex-col items-center justify-center gap-[10px]">
+          <div className="text-[13px] font-semibold">
+            Bookmark Dashboard <span className="text-[9px]">v{version}</span>
+          </div>
+          <div className="text-[11px]">
+            Manage your bookmarks in a simple dashboard
+          </div>
+        </div>
+        <div className="flex justify-center gap-1">
+          <a
+            href="https://github.com/Team-Panopticon/bookmark-dashboard-v3"
+            target="_blank"
+            className="text-[12px] hover:text-sky-700"
+          >
+            Github
+          </a>
+          <span>&nbsp;|&nbsp;</span>
+          <a
+            href="https://github.com/Team-Panopticon/bookmark-dashboard-v3/issues"
+            target="_blank"
+            className="text-[12px] hover:text-sky-700"
+          >
+            Suggestions
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopUp;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   return {
     build: {
       rollupOptions: {
         input: {
           newtab: "./newtab.html",
+          popup: "./popup.html",
         },
       },
       minify: mode === "development" ? false : true,
