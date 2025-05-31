654ec7580ea170fb20a0fd151ed58081e6bfebe8
juyoung.park <juyoung7018.park@gmail.com>
2025-05-31 11:18:13 +0900

    feat: prettier 설정 추가

---
+++ prettier.config.js
@@ -0,0 +1,31 @@
+export default {
+  // 세미콜론을 붙일지 말지 설정
+  semi: true, // 세미콜론을 항상 사용
+
+  // 작은 따옴표 사용
+  singleQuote: true, // 작은 따옴표(') 사용, 기본은 큰 따옴표(")
+
+  // 객체의 마지막 항목에 컴마 추가
+  trailingComma: "es5", // ES5에서 유효한 곳에만 trailing comma를 추가 (객체나 배열 항목 등)
+
+  // 탭 대신 공백 2칸 사용
+  tabWidth: 2, // 탭 대신 공백 2칸
+
+  // 줄바꿈을 위한 공백 설정
+  printWidth: 80, // 한 줄에 최대 80글자
+
+  // 화살표 함수의 괄호 처리
+  arrowParens: "always", // 화살표 함수에 항상 괄호를 추가 (ex. `(x) => x`)
+
+  // 한 줄로 작성할 수 있으면 1줄로 처리
+  proseWrap: "always", // 긴 문장이 자동으로 줄 바꿈
+
+  // JSX에서 속성 값이 문자열일 때 따옴표의 종류 설정
+  jsxSingleQuote: true, // JSX에서 작은 따옴표(') 사용
+
+  // 개행을 할 때 설정
+  bracketSpacing: true, // 객체 리터럴의 중괄호 안에 공백을 추가 (ex. `{ foo: bar }`)
+
+  // 함수 괄호의 공백 설정
+  spaceBeforeFunctionParen: true, // 함수 괄호 앞에 공백 추가 (ex. `function foo () {}`)
+};
